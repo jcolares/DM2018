824a16beb67557ff42c546b5b67d80c1eaf718ee
Jeff <jcolares@gmail.com>
2018-09-15 17:10:47 -0300

    Últimas alterações - após apresentação

--- Jeff/Analise condicoes meteorologicas - parte2.R
+++ Jeff/Analise condicoes meteorologicas - parte2.R
@@ -1,103 +1,104 @@
-# IdentificaÃ§Ã£o das condiÃ§Ãµes meteorolÃ³gicas que causam atrasos em voos em cada aeroporto
+# Identificacao das condicoes meteorologicas que causam atrasos em voos em cada aeroporto
 # utilizando dados curado fornecidos pelo prof.
 
-# Quais sÃ£o as combinaÃ§Ãµes de condiÃ§Ãµes meteorolÃ³gicas que ocorrem com maior frequencia durante os atrasos?
-# Baixa pressÃ£o + temperatura?
+# Quais sao as combinacoes de condicoes meteorologicas que ocorrem com maior frequencia durante os atrasos?
+# Ex: Baixa pressao + temperatura?
 
-# PARTE 2 - PreparaÃ§Ã£o de dados e Data Mining
-
+# PARTE 2 - Preparacao de dados e execucao do Cspade
 
 library(dplyr)
 library(lubridate)
-#library(zoo)
 library(arulesSequences)
-library(arulesViz)
 
 # Linux:
-# setwd("~/github/DM2018")
+setwd("~/github/DM2018")
 # Windows:
-setwd("C:/Users/Jefferson/Documents/GitHub/DM2018")
+# setwd("C:/Users/Jefferson/Documents/GitHub/DM2018")
 
 
-# Carrega minhas funÃ§Ãµes 
+# Carrega minhas funcoes 
 source("Jeff/funkyFunctions.R")
 
 load("datasets/dadosMet.Rdata")
 
-icaoSelect = c("SBJV")
+#Informar os codigos icao dos aeroportos para analise:
+icaoList = c("SBJV", "SBGR", "SBRJ")
 
-# Gera o dataset para o ICAO informado
+# Gera o dataset e roda o CSPADE para os ICAO informados
+for (icaoSelect in icaoList) { 
+  dadosMetICAO = dadosMet %>% 
+    filter(origin==icaoSelect) %>%
+    mutate(timekey = as.numeric(paste(substr(depart_expect,1,4), 
+                                      substr(depart_expect,6,7), 
+                                      substr(depart_expect,9,10), 
+                                      substr(depart_expect,12,13), 
+                                      "00", sep = "")),
+           cancels = ifelse(observation =="XO", 1, 0),
+           delays = ifelse(observation == "WO", 1, 0)) %>%
+    mutate(events = ifelse(cancels + delays > 0 , 1, 0)) %>%
+    mutate(delay_minutes = ifelse(delays == 1, departure_delay, 0)) %>%
+    group_by(timekey) %>%
+    summarise(temperature = mean(depart_temperature, na.rm = TRUE),
+              dif_temp_dew = mean(dif_temp_dew, na.rm = TRUE),
+              humidity = mean(depart_humidity, na.rm = TRUE),
+              pressure = mean(depart_pressure, na.rm = TRUE),
+              visibility = mean(depart_visibility, na.rm = TRUE),
+              avg_delay = mean(delay_minutes, na.rm = TRUE),
+              delays = sum(delays),
+              cancels = sum(cancels))
+  
+  # Flag Eventos (atrasos ou cancelamentos)
+  dadosMetICAO$flag = ifelse(dadosMetICAO$delays + dadosMetICAO$cancels > 0, 1, as.numeric(""))
+  
+  # Identifica sequencias de eventos
+  dadosMetICAO = dadosMetICAO %>%
+    group_by(flag) %>%
+    mutate(eventID=cumsum(flag)) 
+  
+  # Discretizacao
+  dadosMetICAO$temperatureD = ifelse(is.na(dadosMetICAO$temperature), "", paste("T",.bincode(dadosMetICAO$temperature, breaks = seq(0,40,2)), sep = ""))
+  dadosMetICAO$dif_temp_dewD =  ifelse(is.na(dadosMetICAO$dif_temp_dew), 
+                                       "",  
+                                       ifelse(dadosMetICAO$dif_temp_dew==0, 
+                                              "D0",  
+                                              paste("D",.bincode(dadosMetICAO$dif_temp_dew, breaks = seq(0,40,2)), sep = "")))
+  dadosMetICAO$humidityD = ifelse(is.na(dadosMetICAO$humidity), "", paste("H",.bincode(dadosMetICAO$humidity, breaks = seq(0,100,4)), sep = ""))
+  dadosMetICAO$pressureD = ifelse(is.na(dadosMetICAO$pressure), "", paste("P",.bincode(dadosMetICAO$pressure, breaks = seq(1000,1030,2)), sep = ""))
+  dadosMetICAO$visibilityD = ifelse(is.na(dadosMetICAO$visibility), "", paste("V",.bincode(dadosMetICAO$visibility, breaks = seq(0,10,2)), sep = ""))
+  
+  # Gerar sequencias contendo as informacoes de 3 horas anteriores a cada evento
+  dadosMetICAO = get.seq(dadosMetICAO, flag_var = "flag", buffer_size = 4)
+  
+  # Gerar coluna com a combinacao de eventos meteorologicos
+  dadosMetICAO$metConditions = paste(dadosMetICAO$temperatureD, 
+                                     dadosMetICAO$dif_temp_dewD,
+                                     dadosMetICAO$humidityD,
+                                     dadosMetICAO$pressureD,
+                                     dadosMetICAO$visibilityD)
+  
+  # Gerar coluna com a quantidade de itens em cada linha transacao 
+  dadosMetICAO$SIZE = ifelse(gsub("\\s", "", dadosMetICAO$metConditions)=="",0, sapply(strsplit(dadosMetICAO$metConditions, " "), length))
+  
+  #Selecionar colunas relevantes para o CSPADE
+  dadosMetICAO = select(dadosMetICAO, SID, timekey, SIZE, metConditions)
+  colnames(dadosMetICAO) = c("sequenceID", "eventID", "SIZE", "metConditions" )
 
-dadosMetSBRJ = dadosMet %>% 
-  filter(origin==icaoSelect) %>%
-  mutate(timekey = as.numeric(paste(substr(depart_expect,1,4), 
-                                    substr(depart_expect,6,7), 
-                                    substr(depart_expect,9,10), 
-                                    substr(depart_expect,12,13), 
-                                    "00", sep = "")),
-         cancels = ifelse(observation =="XO", 1, 0),
-         delays = ifelse(observation == "WO", 1, 0)) %>%
-  mutate(events = ifelse(cancels + delays > 0 , 1, 0)) %>%
-  mutate(delay_minutes = ifelse(delays == 1, departure_delay, 0)) %>%
-  group_by(timekey) %>%
-  summarise(temperature = mean(depart_temperature, na.rm = TRUE),
-            dif_temp_dew = mean(dif_temp_dew, na.rm = TRUE),
-            humidity = mean(depart_humidity, na.rm = TRUE),
-            pressure = mean(depart_pressure, na.rm = TRUE),
-            visibility = mean(depart_visibility, na.rm = TRUE),
-            avg_delay = mean(delay_minutes, na.rm = TRUE),
-            delays = sum(delays),
-            cancels = sum(cancels))
+  # Gerar Transactios
+  write.table(dadosMetICAO, "tmp/dados.temp.txt", sep=" ", row.names = FALSE, col.names = FALSE, quote = FALSE)
+  metTransactions = read_baskets("tmp/dados.temp.txt", info = c("sequenceID","eventID","SIZE"))
+  
+  # Exectuar o Cspade e armazenar as regras encontradas
+  rules = cspade(metTransactions, control = list(verbose = TRUE))
+  assign(paste("rules", icaoSelect, sep="."), rules)
+  
+  # Armazenar o resultado como dataframe
+  #results = as(rules, "data.frame")
+  #assign(paste("results", icaoSelect, sep="."), results)
+  
+  #registrar nomes de objetos para salvar em arquivo 
+  new_rules = c(paste("rules", icaoSelect, sep="."))
+  if(!exists("rules_list")){rules_list = vector()}
+  rules_list = c(rules_list,  new_rules)
+}  
 
-# Flag Eventos (atrasos ou cancelamentos)
-dadosMetSBRJ$flag = ifelse(dadosMetSBRJ$delays + dadosMetSBRJ$cancels > 0, 1, as.numeric(""))
-
-# Identifica sequencias de eventos
-dadosMetSBRJ = dadosMetSBRJ %>%
-  group_by(flag) %>%
-  mutate(eventID=cumsum(flag)) 
-
-# DiscretizaÃ§Ã£o
-dadosMetSBRJ$temperatureD = ifelse(is.na(dadosMetSBRJ$temperature), "", paste("T",.bincode(dadosMetSBRJ$temperature, breaks = seq(0,40,2)), sep = ""))
-dadosMetSBRJ$dif_temp_dewD =  ifelse(is.na(dadosMetSBRJ$dif_temp_dew), 
-                                     "",  
-                                     ifelse(dadosMetSBRJ$dif_temp_dew==0, 
-                                            "D0",  
-                                            paste("D",.bincode(dadosMetSBRJ$dif_temp_dew, breaks = seq(0,40,2)), sep = "")))
-dadosMetSBRJ$humidityD = ifelse(is.na(dadosMetSBRJ$humidity), "", paste("H",.bincode(dadosMetSBRJ$humidity, breaks = seq(0,100,4)), sep = ""))
-dadosMetSBRJ$pressureD = ifelse(is.na(dadosMetSBRJ$pressure), "", paste("P",.bincode(dadosMetSBRJ$pressure, breaks = seq(1000,1030,2)), sep = ""))
-dadosMetSBRJ$visibilityD = ifelse(is.na(dadosMetSBRJ$visibility), "", paste("V",.bincode(dadosMetSBRJ$visibility, breaks = seq(0,10,2)), sep = ""))
-
-# Gerar sequencias contendo as informaÃ§Ãµes de 3 horas anteriores a cada evento
-dadosMetSBRJ = get.seq(dadosMetSBRJ, flag_var = "flag", buffer_size = 4)
-
-# Gerar coluna com a combinaÃ§Ã£o de eventos meteorologicos
-dadosMetSBRJ$metConditions = paste(dadosMetSBRJ$temperatureD, 
-                                   dadosMetSBRJ$dif_temp_dewD,
-                                   dadosMetSBRJ$humidityD,
-                                   dadosMetSBRJ$pressureD,
-                                   dadosMetSBRJ$visibilityD)
-
-# Gerar coluna com a quantidade de itens em cada linha transaÃ§Ã£o 
-dadosMetSBRJ$SIZE = ifelse(gsub("\\s", "", dadosMetSBRJ$metConditions)=="",0, sapply(strsplit(dadosMetSBRJ$metConditions, " "), length))
-
-#Selecionar colunas relevantes para o CSPADE
-dadosMetSBRJ = select(dadosMetSBRJ, SID, timekey, SIZE, metConditions)
-colnames(dadosMetSBRJ) = c("sequenceID", "eventID", "SIZE", "metConditions" )
-
-
-# Gerar Transactios
-write.table(dadosMetSBRJ, "tmp/dados.temp.txt", sep=" ", row.names = FALSE, col.names = FALSE, quote = FALSE)
-metTransactions = read_baskets("tmp/dados.temp.txt", info = c("sequenceID","eventID","SIZE"))
-
-# Verificar as transaÃ§Ãµes importadas
-as(metTransactions, "data.frame")
-
-# Exectuar o Cspade
-#teste = cspade(metTransactions, parameter = list(support = 0.5), control = list(verbose = TRUE))
-teste = cspade(metTransactions, control = list(verbose = TRUE))
-
-# Verificar o resultado
-resultSBRJ = as(teste, "data.frame")
-save(resultSBRJ, file = "results_cspade.RData")
-
+save(list = rules_list, file = "rules_cspade.RData")
